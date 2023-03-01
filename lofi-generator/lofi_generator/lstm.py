0ba6f31e6b9b4daf39fb90781df75b6efccb7790
Paul Luna <paulluna0215@gmail.com>
2023-03-01 12:06:03 -0800

    implemented predict + train

--- lofi-generator/lofi_generator/lstm.py
+++ lofi-generator/lofi_generator/lstm.py
@@ -3,6 +3,21 @@ Module prepare midi file data and feeds it
 to the Neural Network for training
 """
 
+import glob
+import pickle
+import numpy
+
+from music21 import converter, instrument, note, chord
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.layers import BatchNormalization as BatchNorm
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+
 def train_network():
     """Train a Neural Network to generate music"""
 
@@ -22,19 +37,108 @@ def get_notes():
     Get all the notes and chords from the midi files in
     ./midi_songs direcotry
     """
-    pass
+
+    notes = []
+
+    for file in glob.glob("midi_songs/*.mid"):
+        midi = converter.parse(file)
+
+        print("Parsing %s" % file)
+
+        notes_to_parse = None
+
+        try: # File has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            notes_to_parse = s2.parts[0].recurse()
+        except: # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
+        
+        for element in notes_to_parse:
+
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            
+            elif isinstance(element, chord.Chord):
+                notes.append(".".join(str(n) for n in element.normalOrder))
+
+    
+    with open("data/notes", "wb") as filepath:
+        pickle.dump(notes, filepath)
+    
+    return notes
 
 
 def prepare_sequence(notes, n_vocab):
     """Prepare the sequence used by the Neural Network"""
-    pass
+
+    sequence_length = 32
+
+    # Get all pitch names
+    pitch_names = sorted(set(item for item in notes))
+
+    # Create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitch_names))
+
+    network_input = []
+    network_output = []
+
+    # Create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+    
+    n_patterns = len(network_input)
+
+    # Reshape the input into a format compatible with LSTM layers
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+
+    # Normalize input
+    network_input = network_input / float(n_vocab)
+
+    network_output = np_utils.to_categorical(network_output)
+
+    return (network_input, network_output)
 
 
 def create_network(network_input, n_vocab):
     """Create the structure of the Neural Network"""
-    pass
+    model = Sequential()
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        recurrent_dropout=0.3,
+        return_sequences=True
+    ))
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+    model.add(LSTM(512))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    
+    model.load_weights('lofi-hip-hop-weights-improvement-100-0.6290.hdf5')
+
+    return model
 
 
 def train(model, network_input, network_output):
     """Train the Neural Network"""
-    pass
+    
+    filepath = "updated-lofi-hip-hop-weights-improvement-1{epoch:02d}-{loss:.4f}.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    callbacks_list = [checkpoint]
+    
+    model.fit(network_input, network_output, epochs=100, batch_size=64, callbacks=callbacks_list)
